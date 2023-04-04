7f9aec6dd740755d77161fef3944f7d84878f257
sathiarith <sathiarith_chau@uri.edu>
2023-04-03 22:32:23 -0400

    Moved tests

--- src/bitpack.rs
+++ src/bitpack.rs
@@ -91,26 +91,4 @@ pub fn news(word: u64, width: u64, lsb: u64, value: i64) -> Option<u64> {
 }
 
 
-#[cfg(test)]
-mod tests {
-    use crate::bitpack::fitsu;
-    use crate::bitpack::fitss;
-    #[test]
-    fn fitss_twobits() {
-        assert_eq!(fitss(-2,2), true);
-        assert_eq!(fitss(-1,2), true);
-        assert_eq!(fitss(0,2), true);
-        assert_eq!(fitss(1,2), true);
-        assert_eq!(fitss(2,2), false);
-        assert_eq!(fitss(3,2), false);
-    }
-    #[test]
-    fn fitsu_twobits() {
-        assert_eq!(fitsu(0,2), true);
-        assert_eq!(fitsu(1,2), true);
-        assert_eq!(fitsu(2,2), true);
-        assert_eq!(fitsu(3,2), true);
-        assert_eq!(fitsu(4,2), false);
-        assert_eq!(fitsu(5,2), false);
-    }
-}
+
